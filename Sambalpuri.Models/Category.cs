3c041740ed68a11fa2233c2e82e3a177916daf44
jnana <jnanaranjansahoo016@gmail.com>
2024-07-12 17:37:28 +0530

    Add project files.

---
+++ Sambalpuri.Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sambalpuri.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(30)]
+        public string Name { get; set; }
+    }
+}
